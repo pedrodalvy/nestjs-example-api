22ee4e54ba87e2b1f1972aa383deb69b5a505b12
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-14 14:54:30 -0400

    feat: add User entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import { ObjectType, Field } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@ObjectType()
+@Entity('users')
+export class User {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  name: string;
+
+  @Field()
+  @Column()
+  email: string;
+
+  @Field()
+  @Column()
+  password: string;
+
+  @Field()
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @Field()
+  @CreateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
