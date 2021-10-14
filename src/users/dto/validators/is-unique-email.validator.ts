8fb50f31e79806e8b0f4f060f911fd848cfccd8a
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-14 16:05:58 -0400

    feat: ignore current user id on email validation

--- src/users/dto/validators/is-unique-email.validator.ts
+++ src/users/dto/validators/is-unique-email.validator.ts
@@ -6,15 +6,20 @@ import {
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from '../../repositories/user.repository';
 
+interface IRequest {
+  id?: number;
+}
+
 @ValidatorConstraint({ name: 'uniqueEmail', async: false })
 @Injectable()
 export class IsUniqueEmailValidator implements ValidatorConstraintInterface {
   constructor(private readonly repository: UserRepository) {}
 
   async validate(value: string, args: ValidationArguments): Promise<boolean> {
+    const inputData = { ...args.object } as IRequest;
     const emailExists = await this.repository.findByEmail(value);
 
-    return emailExists === undefined;
+    return emailExists === undefined || emailExists.id === inputData.id;
   }
 
   defaultMessage(args?: ValidationArguments): string {
