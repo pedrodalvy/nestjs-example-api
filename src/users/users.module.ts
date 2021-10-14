05f1c6a0d18c046d551bae426c7643045c8c5216
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-14 15:00:30 -0400

    feat: add users module

---
+++ src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UsersResolver } from './users.resolver';
+import { IsUniqueEmailValidator } from './dto/validators/is-unique-email.validator';
+import { CreateUserService } from './services/create-user.service';
+import { UserRepository } from './repositories/user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
+  providers: [UsersResolver, CreateUserService, IsUniqueEmailValidator],
+})
+export class UsersModule {}
