8aa93985a95ffeefbc0e3075ef3cf0f2ffb16336
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-14 11:31:44 -0400

    feat: add CreateUsers migration

--- src/database/migrations/1634223260329-CreateUsers.ts
+++ src/database/migrations/1634223260329-CreateUsers.ts
@@ -15,26 +15,31 @@ export class CreateUsers1634223260329 implements MigrationInterface {
         name: 'name',
         type: 'varchar',
         length: '255',
+        isNullable: false,
       },
       {
         name: 'email',
         type: 'varchar',
         length: '255',
         isUnique: true,
+        isNullable: false,
       },
       {
         name: 'password',
         type: 'varchar',
         length: '255',
+        isNullable: false,
       },
       {
         name: 'created_at',
         type: 'timestamp with time zone',
         default: 'now()',
+        isNullable: false,
       },
       {
         name: 'updated_at',
         type: 'timestamp with time zone',
+        isNullable: true,
       },
     ],
   });
