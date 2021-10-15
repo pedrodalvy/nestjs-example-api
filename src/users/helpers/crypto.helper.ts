cb24a79ae7ac890ab67ea33353ae685c9a1f5feb
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-15 14:43:26 -0400

    refactor: add type to hashPasswordTransform

--- src/users/helpers/crypto.helper.ts
+++ src/users/helpers/crypto.helper.ts
@@ -1,10 +1,12 @@
 import { hashSync } from 'bcrypt';
+import { ValueTransformer } from 'typeorm/decorator/options/ValueTransformer';
 
-export const hashPasswordTransform = {
+export const hashPasswordTransform: ValueTransformer = {
   to(password: string): string {
     const salt = process.env.PASSWORD_SALT || 8;
     return hashSync(password, Number(salt));
   },
+
   from(hash: string): string {
     return hash;
   },
