9486288b8ae9cbbc12e134ccaa04ee20818af55a
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-15 21:46:21 -0400

    feat: add guards on user resolver

--- src/users/resolvers/users.resolver.ts
+++ src/users/resolvers/users.resolver.ts
@@ -17,6 +17,8 @@ import {
   UpdateUserService,
 } from '../services';
 import { PubSub } from 'graphql-subscriptions';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../../auth/guards/gql-auth.guard';
 
 const pubSub = new PubSub();
 
@@ -41,21 +43,25 @@ export class UsersResolver {
     return user;
   }
 
+  @UseGuards(GqlAuthGuard)
   @Query(() => [User], { name: 'users' })
   async findAll() {
     return this.findAllUsersService.execute();
   }
 
+  @UseGuards(GqlAuthGuard)
   @Query(() => User, { name: 'user' })
   async findOne(@Args('id', { type: () => Int }) id: number) {
     return this.findOneUserService.execute({ id });
   }
 
+  @UseGuards(GqlAuthGuard)
   @Mutation(() => User)
   async updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
     return this.updateUserService.execute(updateUserInput);
   }
 
+  @UseGuards(GqlAuthGuard)
   @Mutation(() => User)
   removeUser(@Args('id', { type: () => Int }) id: number) {
     return this.deleteUserService.execute({ id });
