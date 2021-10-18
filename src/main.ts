8a71c8dbe90cb49ae0436777c7b7516794c5381b
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-18 08:03:55 -0400

    chore: skip missing properties on validator

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({ transform: true, skipMissingProperties: true }),
+  );
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(3000);
 }
